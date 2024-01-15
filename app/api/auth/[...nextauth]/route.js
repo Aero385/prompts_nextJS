79f63d58df5cbb3199acb0e90f114253ae3b5830
Stanislav Prokofjev <Prokostas@gmail.com>
2024-01-15 23:17:44 +0200

    sdf

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -12,41 +12,44 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
-    async session({ session }) {
-        const sessionUser = await User.findOne({
-            email: session.user.email
-        })
-
-        session.user.id = sessionUser._id.toString();
-
-        return session;
-    },
-
-    async signIn({ profile }){
-        try {
-            await connectToDB();
-
-            // check if user exists
-            const userExists = await User.findOne({
-                email: profile.email
+    callbacks: {
+        async session({ session }) {
+            const sessionUser = await User.findOne({
+                email: session.user.email
             })
 
-            if(!userExists) {
-                await User.create({
-                    email: profile.email,
-                    username: profile.name.replace(" ", "").toLowerCase(),
-                    image: profile.image
+            session.user.id = sessionUser._id.toString();
+
+            return session;
+        },
+
+        async signIn({ profile }){
+            try {
+                await connectToDB();
+
+                // check if user exists
+                const userExists = await User.findOne({
+                    email: profile.email
                 })
+
+                if(!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(" ", "").toLowerCase(),
+                        image: profile.image
+                    })
+                }
+
+                // if not, create a new user
+
+                return true;
+            } catch (error) {
+                console.log(error)
+                return false;
             }
-
-            // if not, create a new user
-
-            return true;
-        } catch (error) {
-            console.log(error)
-            return false;
         }
     }
+
 })
 
 export { handler as GET, handler as POST }
